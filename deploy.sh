303d0c589ffc373c87f4d5e2c3a93f44c73e7aab
Marcel Russ <marcel.russ@mehrwert.tech>
2025-02-20 11:49:26 +0100

    added deployment script

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+bundle exec jekyll clean
+bundle exec jekyll build 
+
+git checkout deployment
+
+rm -r _site/tag
+rm -r _site/en/tag
+
+rm -r assets/
+rm -r en/
+rm -r testimonials/
+rm -r trainings/
+
+#mv _site/* .
+
+
